80d60ae8c165803143a09c6429a1c2efc95e2e48
David Ferguson <Fergie_32@hotmail.com>
2019-01-05 22:00:30 +0000

    added tutorial files and new logo

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,9 +1,33 @@
+
 var lessonNumber = 1;
+
+$("#nextTutorial").click(function() { 
+
+    lessonNumber = lessonNumber + 1;
+
+    if (lessonNumber == 1) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial1.html";
+    }  else if (lessonNumber == 2) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial2.html";
+    } else if (lessonNumber == 3) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial3.html";
+    } else if (lessonNumber == 4) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial4.html";
+    } else if (lessonNumber == 5) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial5.html";
+    }  
+});
+
+
+
+
+
+
+
 
 $("#runCode").click(function() { 
     var content = $(text).val();
-    WriteFile(content); 
-    
+    WriteFile(content);    
 });
 
 
