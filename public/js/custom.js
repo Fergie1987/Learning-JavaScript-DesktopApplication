f1a4a19eccea0e614d600476e90aef66b29b982d
David Ferguson <Fergie_32@hotmail.com>
2019-01-06 14:58:33 +0000

    added tutorials 1-4

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,26 @@
 
 var lessonNumber = 1;
 
+$("#previousTutorial").click(function() { 
+
+    if(lessonNumber > 1){
+    lessonNumber = lessonNumber - 1;
+    }
+
+    if (lessonNumber == 1) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial1.html";
+    }  else if (lessonNumber == 2) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial2.html";
+    } else if (lessonNumber == 3) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial3.html";
+    } else if (lessonNumber == 4) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial4.html";
+    } else if (lessonNumber == 5) {
+        document.getElementById('tutorials').src = "./tutorials/tutorial5.html";
+    }  
+});
+
+
 $("#nextTutorial").click(function() { 
 
     lessonNumber = lessonNumber + 1;
@@ -27,7 +47,7 @@ $("#nextTutorial").click(function() {
 
 $("#runCode").click(function() { 
     var content = $(text).val();
-    WriteFile(content);    
+    writeToFile(content);    
 });
 
 
--- rungame/index.js
+++ rungame/index.js
@@ -17,3 +17,68 @@ new b2Vec2(0, 0) //zero gravity
 var CANVAS_WIDTH = 1000;
 var CANVAS_HEIGHT = 600;
 var SCALE = 30; 
+
+var debugDraw = new b2DebugDraw(); 
+debugDraw.SetSprite(document.getElementById("can").getContext("2d")); 
+debugDraw.SetDrawScale(SCALE); 
+debugDraw.SetFillAlpha(0.3); 
+debugDraw.SetLineThickness(1.0); 
+debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit); 
+world.SetDebugDraw(debugDraw); 
+
+var fixDef = new b2FixtureDef; 
+fixDef.density = 1.0; 
+fixDef.friction = 0.5; 
+fixDef.restitution = 0; 
+fixDef.shape = new b2PolygonShape; 
+fixDef.shape.SetAsBox((CANVAS_WIDTH/SCALE)/2, (100/SCALE)/2); 
+var bodyDef = new b2BodyDef; 
+bodyDef.type = b2Body.b2_staticBody; 
+bodyDef.position.x = CANVAS_WIDTH/2/SCALE; 
+bodyDef.position.y = CANVAS_HEIGHT/SCALE - (20/SCALE); 
+var bottom = world.CreateBody(bodyDef).CreateFixture(fixDef); 
+bottom.GetBody().SetUserData({id: "bottom"}) 
+
+var fixDefTop = new b2FixtureDef;
+fixDefTop.density = 1.0;
+fixDefTop.friction = 0.5;
+fixDefTop.restitution = 0;
+fixDefTop.shape = new b2PolygonShape;
+fixDefTop.shape.SetAsBox((CANVAS_WIDTH/SCALE)/2, (100/SCALE)/2);
+var bodyDefTop = new b2BodyDef;
+bodyDefTop.type = b2Body.b2_staticBody;
+bodyDefTop.position.x = (CANVAS_WIDTH/2/SCALE);
+bodyDefTop.position.y = (CANVAS_HEIGHT/SCALE) - (580/SCALE);
+var canvasTop = world.CreateBody(bodyDefTop).CreateFixture(fixDefTop);
+canvasTop.GetBody().SetUserData({id: "canvasTop"}) 
+var fixDefSide = new b2FixtureDef; 
+fixDefSide.density = 1.0;
+fixDefSide.friction = 0.5;
+fixDefSide.restitution = 0;
+fixDefSide.shape = new b2PolygonShape;
+fixDefSide.shape.SetAsBox((200/SCALE)/2, (CANVAS_HEIGHT/SCALE)/2);
+var bodyDefRight = new b2BodyDef;
+bodyDef.type = b2Body.b2_staticBody;
+bodyDefRight.position.x = (CANVAS_WIDTH-20) / SCALE;
+bodyDefRight.position.y = (CANVAS_HEIGHT/SCALE)/2;
+var right = world.CreateBody(bodyDefRight).CreateFixture(fixDefSide);
+right.GetBody().SetUserData({id: "right"})
+
+var bodyDefLeft = new b2BodyDef;
+bodyDef.type = b2Body.b2_staticBody;
+bodyDefLeft.position.x = (20/SCALE);
+bodyDefLeft.position.y = (CANVAS_HEIGHT/SCALE)/2; 
+var left = world.CreateBody(bodyDefLeft).CreateFixture(fixDefSide); 
+left.GetBody().SetUserData({id: "left"})
+
+function update() {
+world.Step (
+1/60 //frame-rate
+, 10 //velocity iterations
+, 10 //position iterations
+);
+world.DrawDebugData();
+world.ClearForces();
+window.requestAnimationFrame(update);
+}
+window.requestAnimationFrame(update); 
